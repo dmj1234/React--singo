9f6e2ff2d4a154d480d6c809483ad5793cc98298
dmj1234 <you@example.com>
2021-01-03 17:23:57 +0800

    搜集表单数据

--- src/views/Singo.tsx
+++ src/views/Singo.tsx
@@ -1,29 +1,56 @@
 import Layout from '../components/Layout';
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import {TagsSection} from './Singo/TagsSection';
 import {NoteSection} from './Singo/NotesSection';
 import {CategorySection} from './Singo/CategorySection';
 import {NumberPadSection} from './Singo/NumberPadSection';
 
-
-
-
-
-
 const MyLayout = styled(Layout)`
   display: flex;
   flex-direction: column;
 `
+type Category = '-'| '+'
 
 function Singo() {
+    const [selected,setSelected] = useState({
+        tags:[] as string[],
+        note:'',
+        category:'-' as Category,
+        amount: 0
+    })
     return (
         <MyLayout>
-            <TagsSection/>
-            <NoteSection/>
-            <CategorySection/>
-            <NumberPadSection/>
+            <TagsSection value ={selected.tags}
+                         onChange={(tags) =>setSelected({
+                             ...selected,
+                             tags:tags
+                         })}/>
+            <NoteSection value = {selected.note}
+            onChange={(note)=>{
+                setSelected({
+                    ...selected,
+                    note:note
+                })
+            }}/>
+            <CategorySection value={selected.category}
+            onChange={(category)=>{
+                setSelected({
+                    ...selected,
+                    category:category
+                });
+            }}
+            />
+            <NumberPadSection value={selected.amount}
+            onChange={(amount)=>{
+                setSelected({
+                ...selected,
+                amount:amount
+            })
+            }}
+                              onOk={()=>{}}
+            />
         </MyLayout>
     );
-}
+};
 export default Singo;
