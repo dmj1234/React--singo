f58591f8ef6552ffb9d419d7355dc34f20928f89
dmj1234 <you@example.com>
2021-01-04 11:19:15 +0800

    创建/tags/tag:路由  学会exact的用法

--- src/views/tags.tsx
+++ src/views/tags.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {useTags} from './Singo/useTags';
 import styled from 'styled-components';
 import Icon from '../components/icon';
+import { Link } from 'react-router-dom';
 const Button = styled.button`
 font-size: 18px;border: none; padding: 8px 12px;
 `
@@ -12,11 +13,13 @@ font-size: 16px;
   >li{
     border-bottom: 1px solid #d5d5d0;
     line-height: 20px;
-    padding: 12px 16px 12px 0;
     margin-left: 16px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+    >a{
+      padding: 12px 16px 12px 0;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
   }
 `
 const Center =styled.div`
@@ -35,8 +38,10 @@ function Tags() {
            <TagList>
                {tags.map(tag =>
                    <li key={tag}>
-                       <span>{tag}</span>
-                       <Icon name="right"/>
+                       <Link to={'/tags/' +tag}>
+                           <span className="oneLine">{tag}</span>
+                           <Icon name="right"/>
+                       </Link>
                    </li>)}
            </TagList>
             <Center>
