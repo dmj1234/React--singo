a4b925b382dee464b6793e68c955c7a4f8b9a412
dmj1234 <you@example.com>
2021-01-04 15:44:21 +0800

    创建createId  让Id自增

--- src/views/Singo/TagsSection.tsx
+++ src/views/Singo/TagsSection.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import {useTags} from './useTags';
+import {createId} from '../../lib/createId';
 
 const Wrapper = styled.section`
     background: #FFFFFF;padding: 12px 16px;
@@ -31,7 +32,7 @@ const selectedTagIds = props.value;
     const  onAddTag = () =>{
         const  tagName = window.prompt('新标签的名称为');
         if(tagName!==null){
-            setTags([...tags,{id:Math.random(),name:tagName}])
+            setTags([...tags,{id:createId(),name:tagName}])
         }
     };
     const onToggleTag = (tagId:number) => {
