58e1c2c02f19bd4a0793b1d9a6148d785b1ce032
dmj1234 <you@example.com>
2021-01-04 11:02:07 +0800

    TagList  初步完成

--- src/views/tags.tsx
+++ src/views/tags.tsx
@@ -1,12 +1,50 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import {useTags} from './Singo/useTags';
+import styled from 'styled-components';
+import Icon from '../components/icon';
+const Button = styled.button`
+font-size: 18px;border: none; padding: 8px 12px;
+`
+const  TagList = styled.ol`
+font-size: 16px;
+  background: white;
+  >li{
+    border-bottom: 1px solid #d5d5d0;
+    line-height: 20px;
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`
+const Center =styled.div`
+display: flex;
+  justify-content: center;
+align-items: center;
+  flex-direction: column;
+`
+const Space = styled.div`
+height: 16px`
 
 function Tags() {
     const {tags,setTags} = useTags()
     return (
         <Layout>
-            <h2>标签页面</h2>
+           <TagList>
+               {tags.map(tag =>
+                   <li key={tag}>
+                       <span>{tag}</span>
+                       <Icon name="right"/>
+                   </li>)}
+           </TagList>
+            <Center>
+                <Space/>
+                <Space/>
+                <Space/>
+                <Button>新增标签</Button>
+            </Center>
         </Layout>
     );
 }
